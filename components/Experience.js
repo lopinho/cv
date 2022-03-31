91a024bd5835770e2a63e134f6a7ad485731b4c4
André Ferreira <lopinho@gmail.com>
2022-03-31 13:59:36 -0300

    acertando configuração do next

--- components/Experience.js
+++ components/Experience.js
@@ -14,7 +14,7 @@ const Experience = (props) => {
                       <CardBody>
                           <CardTitle tag="h4" className="fw-lighter">BRBid.com</CardTitle>
                           <CardSubtitle tag="h6" className="fw-lighter mb-3">Tech lead</CardSubtitle>
-                          <CardText className=" fw-lighter lh-sm" style={{ "font-size": "0.9em" }}>
+                          <CardText className=" fw-lighter lh-sm">
                               Auction site that has sold more than 200,000 vehicles on its marketplace with more than 6 million monthly visits.
                               Considered in 2017 by most customers one of the best real-time auction sites in Brazil.
                               Made with Django and nodejs using realtime (socket.io) for backend and Angular/React for frontend. I was responsible for ensuring scalability and assembling the entire system infrastructure on AWS,
--- components/Projects.js
+++ components/Projects.js
@@ -13,12 +13,12 @@ const Projects = (props) => {
                   <Card className="shadow p-2">
                       <Row>
                           <Col md="4" className="d-flex flex-column justify-content-center">
-                              <CardImg top width="300" src="../images/brbid_site.png" alt="Card image cap" className="img-fluid rounded-start" />
+                              <CardImg top width="300" src="images/brbid_site.png" alt="Card image cap" className="img-fluid rounded-start" />
                           </Col>
                           <Col md="8">
                               <CardBody className="">
                                   <CardTitle className="h4 fw-lighter text-center mb-3">BRBid.com</CardTitle>
-                                  <CardText className="w-75 fw-lighter lh-sm mx-auto" style={{ "font-size": "0.9em" }}>
+                                  <CardText className="w-75 fw-lighter lh-sm mx-auto">
                                       Auction site that has sold more than 200,000 vehicles on its marketplace with more than 6 million monthly visits.
                                       Considered in 2017 by most customers one of the best real-time auction sites in Brazil.
                                       Made with Django and nodejs using realtime (socket.io) for backend and Angular/React for frontend. I was responsible for ensuring scalability and assembling the entire system infrastructure on AWS,
